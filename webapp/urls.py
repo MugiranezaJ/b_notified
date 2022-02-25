ed8076458a75ff3caac05b1000ad6e3f14943e86
MugiranezaJ <jackswalter7@gmail.com>
2022-02-25 16:23:50 +0200

    removing comments

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.send_email_notification, name="index"),
     path('notify/', views.notify, name="notify")
 ]
